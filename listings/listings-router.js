e347bfac5aba194dacf95c94e864010dd5b8b90d
Karen Lei <karenwinnielei@gmail.com>
2020-07-28 15:47:40 -0700

    fix delete endpoint for listings and update README with endpoint instructions

--- listings/listings-router.js
+++ listings/listings-router.js
@@ -49,7 +49,7 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', (req, res) => {
   const { id } = req.params;
 
-  db('listings')
+  db('features')
     .where({ id })
     .del()
     .then((count) => {
