94cd6699d2d46943b1006add0b4f06073e8b7683
Karen Lei <karenwinnielei@gmail.com>
2020-07-30 10:58:56 -0700

    add testing for auth-router

--- auth/auth-router.test.js
+++ auth/auth-router.test.js
@@ -2,29 +2,118 @@ const supertest = require('supertest');
 
 const server = require('../api/server');
 
+const db = require('../database/connection');
+const Users = require('../users/users-model');
+
 const authRouter = require('../auth/auth-router');
+
+describe('environment', function () {
+  it('should be using the testing database', function () {
+    expect(process.env.DB_ENV).toBe('testing');
+  });
+});
 
 describe('router', function () {
   it('runs the tests', function () {
     expect(true).toBe(true);
   });
 
-  describe('POST /', function () {
+  const credentials = {
+    email: 'email@test.com',
+    password: 'test',
+  };
+
+  describe('POST / with new email', function () {
+    beforeEach(async () => {
+      await db('users').truncate();
+    });
+
     it('should respond with 201 OK', async function () {
       await supertest(server)
         .post('/api/auth/register')
-        .send({ email: 'email@test.com', password: 'test' })
+        .send(credentials)
         .then((res) => {
           expect(res.status).toBe(201);
         });
     });
+  });
 
+  describe('POST / with duplicate email', function () {
     it('should respond with 500 if email is not unique', async function () {
       await supertest(server)
         .post('/api/auth/register')
-        .send({ email: 'email@test.com', password: 'test' })
+        .send(credentials)
         .then((res) => {
           expect(res.status).toBe(500);
+        });
+    });
+  });
+
+  describe('POST / with login', function () {
+    it('should respond with 200 if credentials are valid', async function () {
+      await supertest(server)
+        .post('/api/auth/login')
+        .send(credentials)
+        .then((res) => {
+          expect(res.status).toBe(200);
+        });
+    });
+
+    it('should respond with 401 if credentials are invalid', async function () {
+      await supertest(server)
+        .post('/api/auth/login')
+        .send({ email: 'email@test.com', password: 'error' })
+        .then((res) => {
+          expect(res.status).toBe(401);
+        });
+    });
+
+    it('should respond with 400 if missing email', async function () {
+      await supertest(server)
+        .post('/api/auth/login')
+        .send({ password: 'test' })
+        .then((res) => {
+          expect(res.status).toBe(400);
+        });
+    });
+
+    it('should respond with 400 if missing password', async function () {
+      await supertest(server)
+        .post('/api/auth/login')
+        .send({ email: 'email@email.com' })
+        .then((res) => {
+          expect(res.status).toBe(400);
+        });
+    });
+  });
+
+  describe('PUT / with login', function () {
+    it('should respond with 201 with updated credentials', async function () {
+      await supertest(server)
+        .put('/api/auth/login/1')
+        .send({ email: 'email@email.com', password: 'test' })
+        .then((res) => {
+          expect(res.status).toBe(201);
+        });
+    });
+  });
+
+  describe('GET / with login', function () {
+    it('shouldr respond with 200 with login page', async function () {
+      await supertest(server)
+        .get('/api/auth/login')
+        .then((res) => {
+          expect(res.status).toBe(200);
+        });
+    });
+  });
+
+  describe('GET / with register', function () {
+    it('should respond with 200 with registration page', async function () {
+      await supertest(server)
+        .get('/api/auth/register')
+        .then((res) => {
+          expect(res.status).toBe(200);
         });
     });
   });
---
+++ listings/listings-router.test.js
@@ -0,0 +1,111 @@
+const supertest = require('supertest');
+
+const server = require('../api/server');
+
+const db = require('../database/connection');
+const Users = require('../users/users-model');
+const Listings = require('../listings/listings-model');
+
+const listingsRouter = require('../listings/listings-router');
+
+let token;
+
+const credentials = {
+  email: 'email@test.com',
+  password: 'test',
+};
+
+beforeAll((done) => {
+  supertest(server)
+    .post('/api/auth/login')
+    .send(credentials)
+    .end((err, response) => {
+      token = response.body.token; // save the token!
+      done();
+    });
+});
+
+describe('GET /', () => {
+  // token not being sent - should respond with a 401
+  test('It should require authorization', () => {
+    return supertest(server)
+      .get('/api/listings')
+      .then((response) => {
+        expect(response.statusCode).toBe(401);
+      });
+  });
+  // send the token - should respond with a 200
+  test('It responds with JSON', () => {
+    return supertest(server)
+      .get('/api/listings')
+      .set('Authorization', token)
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.type).toBe('application/json');
+      });
+  });
+});
+
+// describe('router', function () {
+//   it('runs the tests', function () {
+//     expect(true).toBe(true);
+//   });
+
+//   const features = {
+//     host_about_len:
+//       'My wife and I own this house and rent out the guest rooms on weekends',
+//     description_len: 'A quiet house in north seattle',
+//     property_type: 'House',
+//     neighbourhood: 'Silver Lake',
+//     city: 'Everett',
+//     state: 'WA',
+//     zipcode: '98208',
+//     bathrooms: 1.75,
+//     bedrooms: 3,
+//     beds: 6,
+//     accommodates: 6,
+//     guests_included: 2,
+//     square_feet: '1200',
+//     cancellation_policy: 'moderate',
+//     instant_bookable: 't',
+//     is_business_travel_ready: 'f',
+//     review_scores_rating: 90,
+//     number_of_reviews: 4,
+//     transit_len: 'There is a bus stop at the end of the street!',
+//     name: 'Silver Lake House',
+//   };
+
+//   const credentials = {
+//     email: 'email@test.com',
+//     password: 'test',
+//   };
+
+//   describe('GET /', function () {
+//     it('should respond with 201 OK', async function () {
+//       await supertest(server)
+//         .post('/api/auth/register')
+//         .send(credentials)
+//         .then((res) => {
+//           expect(res.status).toBe(201);
+//         });
+//     });
+
+//     it('should respond with 200 if credentials are valid', async function () {
+//       await supertest(server)
+//         .post('/api/auth/login')
+//         .send(credentials)
+//         .then((res) => {
+//           expect(res.status).toBe(200);
+//         });
+//     });
+
+//     it('should respond with 200 with listings', async function () {
+//       await supertest(server)
+//         .get('/api/listings')
+//         .send(req.headers.authorization)
+//         .then((res) => {
+//           expect(res.status).toBe(200);
+//         });
+//     });
+//   });
+// });
